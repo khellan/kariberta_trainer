75fcce03eb2a944b90832db7f954c1405d2dc459
Knut Hellan <knut@lysesvart.no>
2020-03-11 10:20:51 +0100

    Training optimization
    Use apex (fp16) for performance.
    Train for 5 epochs
    Use line_by_line to reduce memory requirements
    Set per_gpu_trian_batch_size to 4 to avoid GPUs running out of memory.

--- train.sh
+++ train.sh
@@ -1,9 +1,5 @@
 export TRAIN_FILE=data/train_sentences.txt
 export TEST_FILE=data/test_sentences.txt
-#export TRAIN_FILE=data/train/t5.txt
-#export TEST_FILE=data/test/t5.txt
-#export TRAIN_FILE=data/train_subset_0.txt
-#export TEST_FILE=data/test_subset_0.txt
 
 pipenv run python run_language_modeling.py \
     --output_dir ./models/KariBERTa-small \
@@ -14,14 +10,14 @@ pipenv run python run_language_modeling.py \
     --do_train \
     --do_eval \
     --learning_rate 1e-4 \
-    --num_train_epochs 50 \
+    --fp16 \
+    --num_train_epochs 5 \
     --save_total_limit 2 \
     --save_steps 2000 \
-    --per_gpu_train_batch_size 16 \
-    --evaluate_during_training \
+    --line_by_line \
+    --per_gpu_train_batch_size 4 \
     --seed 42 \
     --train_data_file $TRAIN_FILE \
     --eval_data_file $TEST_FILE
 
-#    --fp16 \
 
